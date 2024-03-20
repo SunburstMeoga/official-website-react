fdd0961415996711829f2a456c4550032107fe80
SunburstMeoga <1025113986@qq.com>
2024-03-20 18:17:54 +0800

    config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/home'
+import Home from '@/pages/home'
+import '@/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>this is home page</div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
 }
 
