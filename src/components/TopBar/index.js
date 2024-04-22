349cf84ed6563bcdf60ea77c892b422c4293cbd1
SunburstMeoga <1025113986@qq.com>
2024-04-22 18:09:46 +0800

    pc top bar

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -147,41 +147,40 @@ const TopBar = () => {
             {/* pc top bar */}
             <div className="hidden md:block">
                 <div className="flex justify-center items-center w-full fixed z-10">
-                    <div className="w-11/12 flex justify-between items-center">
+                    <div className="w-full flex justify-between items-center px-3-8 py-2-4">
                         <div className="flex justify-center items-center">
                             <div className="flex justify-start items-center">
                                 <div>
-                                    <img className="w-20" alt="" src="/images/logo.png"></img>
+                                    <img className="w-6-1" alt="" src="/images/logo.png"></img>
                                 </div>
                             </div>
-                            <div className="ml-10 flex justify-start items-center">
+                            <div className="flex justify-start items-center">
                                 {menuList.map((item, index) => {
                                     return (
                                         <div className="relative py-8" key={index}>
                                             <div onMouseEnter={() => onMenuItemEnter(item, index)} onClick={() => clickMenuItem(item, index)} key={index}
                                                 className={["font-semibold",
                                                     "cursor-pointer",
-                                                    "ml-10",
+                                                    "ml-3-7",
                                                     "border-b-2",
-                                                    "pb-1",
+                                                    "pb-0-1",
                                                     "menu-item",
+                                                    "text-1-3",
                                                     index === menuList.length - 1 ? "relative z-10" : '',
                                                     clickedMenu === item.name ? "border-primary-green text-primary-green" : "border-transparent text-word-gray"].join(' ')}>
                                                 {item.title}
                                             </div>
 
                                             {(item.hasChild && mouseEnterMenu === 'solution') &&
-                                                <div className="absolute text-sm font-normal px-8 pt-3 pb-2 text-left top-16 left-8  rounded shadow-2xl bg-white text-model-gray z-50"
+                                                <div className="absolute font-medium px-2-3 text-1-5 text-left leading-point-114 py-1-6 rounded shadow-2xl bg-white text-model-gray z-50"
                                                     onMouseLeave={(e) => onMouseLeaveSecondMenu(e)}>
                                                     {item.children.map((_item, _index) => {
                                                         return (
                                                             <div onMouseEnter={() => onMouseEnterSecondMenu(_item, _index)}
                                                                 onClick={() => clickMenuItem(_item, _index)}
                                                                 className={[
-                                                                    "w-52",
-                                                                    "py-1",
-                                                                    "pl-2",
-                                                                    "mb-2",
+                                                                    "w-22-2",
+                                                                    "py-1-0",
                                                                     "cursor-pointer",
                                                                     _index === mouseEnterSecondMenu ? "text-active-color" : ""].join(" ")} key={_index}>
                                                                 {_item.title}
@@ -189,29 +188,19 @@ const TopBar = () => {
                                                         )
                                                     })}
                                                 </div>}
-
-
-                                            {/* <div onMouseEnter={() => onMenuItemEnter(item, index)} onClick={() => clickMenuItem(item, index)} key={index}
-                                                className={["font-semibold",
-                                                    "cursor-pointer", "ml-10", "border-b-2", "pb-1", "menu-item",
-                                                    index === menuList.length - 1 ? "relative" : '',
-                                                    clickedMenu === item.name ? "border-primary-green text-primary-green" : "border-transparent text-word-gray"].join(' ')}>
-                                                <div className="relative z-10">{item.title}</div>
-                                                
-                                            </div> */}
                                         </div>
                                     )
                                 })}
                             </div>
                         </div>
                         <div className="flex justify-end items-center">
-                            <div className="flex justify-between items-center mr-6 cursor-pointer">
-                                <div className="title-gray">Sign in</div>
-                                <div className="w-4 ml-2">
+                            <div className="flex justify-between items-center mr-2-0 cursor-pointer">
+                                <div className="text-title-gray text-1-0">Sign in</div>
+                                <div className="w-1-0 ml-1-0">
                                     <img src="/images/__after.png" alt=""></img>
                                 </div>
                             </div>
-                            <div className="green-button rounded-full px-6 py-3 cursor-pointer">中文 / English</div>
+                            <div className="green-button rounded-full w-9-5 h-3-1 cursor-pointer flex justify-items-center">中文 / English</div>
                         </div>
                     </div>
                 </div>
