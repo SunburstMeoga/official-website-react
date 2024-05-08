35221679df8d5875ea95fb930ceb0446637370d9
SunburstMeoga <1025113986@qq.com>
2024-05-08 21:40:55 +0800

    loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,11 +3,14 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className='w-full h-svh flex flex-col justify-center items-center'>
-            <div className='w-1/4 lg:w-1/12 lg:hidden'>
+            {/* <div className='w-1/4 lg:w-1/12 lg:hidden'>
                 <img src='/images/logo.png'></img>
-            </div>
+            </div> */}
             <div className='w-full lg:w-1/4 -mt-4-0 lg:mt-0-1'>
                 <img src='/images/loading.gif'></img>
+            </div>
+            <div className='text-primary-green font-bold text-2-0'>
+                Loading...
             </div>
         </div>
     )
