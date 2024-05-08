7a3f95509ded679e4f06297237f7d4c0ba1d96c5
SunburstMeoga <1025113986@qq.com>
2024-05-08 23:15:53 +0800

    style

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -6,9 +6,9 @@ const Loading = () => {
             {/* <div className='w-1/4 lg:w-1/12 lg:hidden'>
                 <img src='/images/logo.png'></img>
             </div> */}
-            <div className='w-full lg:w-1/4 -mt-4-0 lg:mt-0-1'>
+            {/* <div className='w-full lg:w-1/4 -mt-4-0 lg:mt-0-1'>
                 <img src='/images/loading.gif'></img>
-            </div>
+            </div> */}
             <div className='text-primary-green font-bold text-2-0'>
                 Loading...
             </div>
