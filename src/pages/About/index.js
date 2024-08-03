8776c37cac3771a84b686cdafe3b93a01966f6b1
SunburstMeoga <1025113986@qq.com>
2024-08-04 01:35:26 +0800

    chart

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -69,7 +69,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="w-full lg:bg-home-bg  lg:justify-center lg:items-center lg:hidden">
+                <div className="w-full lg:bg-home-bg  lg:justify-center lg:items-center ">
                     <div className="w-full flex flex-col justify-start items-center mt-2-2 pt-1-4 lg:flex-row lg:w-90-9 lg:justify-between lg:mr-auto lg:ml-auto ">
                         <div className="lg:flex lg:justify-between lg:w-59-6 ">
                             <div className="w-23-5 lg:w-28-1 lg:h-16-0 bg-white rounded-2xl box-shadow-green white-opaity-40">
